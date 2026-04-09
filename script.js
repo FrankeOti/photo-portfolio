4320e81e3e2896fd10288c2b821724fe7bc34fd5
Frankline Otieno <otienofranklineldk@gmail.com>
2026-04-09 10:47:27 +0300

    Dawn

--- script.js
+++ script.js
@@ -3,5 +3,29 @@ function toggleMenu(icon) {
 
   const menu = document.getElementById("menu");
   menu.classList.toggle("active");
+
+  const title = document.querySelector(".site-title");
+  title.classList.toggle("dark");
+}
+
+
+
+function openLightbox(img) {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+
+  lightbox.style.display = "flex";
+  setTimeout(() => lightbox.classList.add("show"), 10);
+
+  lightboxImg.src = img.src;
+}
+
+function closeLightbox() {
+  const lightbox = document.getElementById("lightbox");
+  lightbox.classList.remove("show");
+
+  setTimeout(() => {
+    lightbox.style.display = "none";
+  }, 300);
 }
 
